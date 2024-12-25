dee50c939f8553ef30c0972362dbf9aa02700098
OlufunbiIK <olufunbiibrahim@gmail.com>
2024-12-25 14:59:13 +0100

    been trying to host both client and server together since 12

--- src/components/BlogPage.jsx
+++ src/components/BlogPage.jsx
@@ -8,72 +8,71 @@ import CategorySelection from "./CategorySelection";
 import SideBar from "./SideBar";
 
 export default function BlogPage() {
-	const [blogs, setBlogs] = useState([]);
-	const [currentPage, setCurrentPage] = useState(1);
-	const pageSize = 12; //blogs per page
-	const [selectedCategory, setSelectedCategory] = useState(null);
-	const [activeCategory, setActiveCategory] = useState(null);
-	useEffect(() => {
-		async function fetchBlogs() {
-			let url =
-				"http://localhost:9000/blogs?pages=${currentPage}&limit=${pageSize}";
+  const [blogs, setBlogs] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 12; //blogs per page
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [activeCategory, setActiveCategory] = useState(null);
+  useEffect(() => {
+    async function fetchBlogs() {
+      let url = `https://new-blog-4tiovtjp1-olufunbiiks-projects.vercel.app/blogs?page=${currentPage}&limit=${pageSize}`;
 
-			// if filter by category
-			if (selectedCategory) {
-				url += `$category="${selectedCategory}`;
-			}
-			const response = await fetch(url);
-			const data = await response.json();
-			setBlogs(data);
-		}
-		fetchBlogs();
-	}, [currentPage, pageSize, selectedCategory]);
+      // if filter by category
+      if (selectedCategory) {
+        url += `$category="${selectedCategory}`;
+      }
+      const response = await fetch(url);
+      const data = await response.json();
+      setBlogs(data);
+    }
+    fetchBlogs();
+  }, [currentPage, pageSize, selectedCategory]);
 
-	//page changing button
-	function handlePageChange(pageNumber) {
-		setCurrentPage(pageNumber);
-	}
+  //page changing button
+  function handlePageChange(pageNumber) {
+    setCurrentPage(pageNumber);
+  }
 
-	const handleCategoryChange = (category) => {
-		setSelectedCategory(category);
-		setCurrentPage(1);
-		setActiveCategory(category);
-	};
-	return (
-		<div>
-			{/* category section */}
-			<div>
-				<CategorySelection
-					onSelectCategory={handleCategoryChange}
-					selectedCategory={selectedCategory}
-					activeCategory={activeCategory}
-				/>
-			</div>
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category);
+    setCurrentPage(1);
+    setActiveCategory(category);
+  };
+  return (
+    <div>
+      {/* category section */}
+      <div>
+        <CategorySelection
+          onSelectCategory={handleCategoryChange}
+          selectedCategory={selectedCategory}
+          activeCategory={activeCategory}
+        />
+      </div>
 
-			{/* blogCards section */}
-			<div className="flex flex-col lg:flex-row gap-12">
-				{/* blog cards component */}
-				<BlogCards
-					blogs={blogs}
-					currentPage={currentPage}
-					selectedCategory={selectedCategory}
-					pageSize={pageSize}
-				/>
-				{/* sidebar component */}
-				<div>
-					<SideBar />
-				</div>
-			</div>
+      {/* blogCards section */}
+      <div className="flex flex-col lg:flex-row gap-12">
+        {/* blog cards component */}
+        <BlogCards
+          blogs={blogs}
+          currentPage={currentPage}
+          selectedCategory={selectedCategory}
+          pageSize={pageSize}
+        />
+        {/* sidebar component */}
+        <div>
+          <SideBar />
+        </div>
+      </div>
 
-			{/* paginations */}
-			<div>
-				<Paginations
-					onPageChange={handlePageChange}
-					currentPage={currentPage}
-					blogs={blogs}
-					pageSize={pageSize}
-				/>
-			</div>
-		</div>
-	);
+      {/* paginations */}
+      <div>
+        <Paginations
+          onPageChange={handlePageChange}
+          currentPage={currentPage}
+          blogs={blogs}
+          pageSize={pageSize}
+        />
+      </div>
+    </div>
+  );
 }
